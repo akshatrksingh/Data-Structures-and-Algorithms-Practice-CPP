982258029b5979e180f6bbf3595d70e6a0eed8d0
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2021-08-29 21:03:04 +0530

    Update Interviewbit-XOR-ingTheSubarrays.cpp

--- Interviewbit-XOR-ingTheSubarrays.cpp
+++ Interviewbit-XOR-ingTheSubarrays.cpp
@@ -3,7 +3,7 @@ int Solution::solve(vector<int> &v)
     int x;
     for(int i = 0; i < v.size(); i++)
     {
-        if((i+1)*(v.size()-i) % 2 != 0)
+        if((i+1)*(v.size()-i) % 2 != 0)  //each element will contribute to (i+1)*(v.size()-1) contiguous subarrays
             x ^= v[i];
     }
     return x;
