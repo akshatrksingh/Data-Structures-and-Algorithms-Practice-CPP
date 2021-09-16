6060fc904317b934f178c134cc3acb3e7310507a
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2021-09-17 00:00:58 +0530

    Add files via upload

--- Basics-LinkedListInsertion.cpp
+++ Basics-LinkedListInsertion.cpp
@@ -14,9 +14,25 @@ public:
 	}
 };
 
-void build()
+void display(node *head)
 {
+	while(head != NULL)
+	{
+		cout<<head->data<<" ";
+		head = head->next;
+	}
+	cout<<"\n";
+}
 
+int length(node *head)
+{
+	int len = 0;
+	while(head != NULL)
+	{
+		len++;
+		head = head->next;
+	}
+	return len;
 }
 
 void insertAtBeg(node *&head, int d)
@@ -26,27 +42,69 @@ void insertAtBeg(node *&head, int d)
 		head = new node(d);
 		return;
 	}
-
 	node *n = new node(d);
 	n->next = head; // (*n).next = head;
 	head = n; 
 }
 
-void display(node *head)
+void insertAtEnd(node *&head, int d)
 {
-	while(head != NULL)
+	if(head == NULL)
 	{
-		cout<<head->data<<" ";
-		head = head->next;
+		head = new node(d);
+		return;
+	}
+
+	node *end = head;
+	while(end->next != NULL)
+	{
+		end = end->next;
+	}
+	end->next = new node(d);
+}
+
+void insertAtKthNode(node *&head, int d, int k)
+{
+	if(head == NULL || k == 0)
+	{
+		insertAtBeg(head, d);
+	}
+	else if(k > length(head))
+	{
+		insertAtEnd(head, d);
+	}
+	else
+	{
+		int jump = 1;
+		node *tmp = head;
+		while(jump <= k-1)
+		{
+			tmp = tmp->next;
+			jump++;
+		}
+		node *n = new node(d);
+		n->next = tmp->next;
+		tmp->next = n; 
 	}
 }
+
 
 int main()
 {
 	node *head = NULL;
 	insertAtBeg(head, 21);
-	insertAtBeg(head, 5);
-	insertAtBeg(head, 2);
-	insertAtBeg(head, 54);
 	display(head);
+	insertAtBeg(head, 231);
+	display(head);
+	insertAtKthNode(head, 5, 1);
+	display(head);
+	insertAtKthNode(head, 62, 2);
+	display(head);
+	insertAtKthNode(head, 5222, 3);
+	display(head);
+	insertAtKthNode(head, 223, 4);
+	display(head);
+	insertAtEnd(head, 53);
+	display(head);
+	return 0;
 }
