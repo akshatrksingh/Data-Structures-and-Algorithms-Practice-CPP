c78c23107815107018ce11192b20e82efc53016a
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-06-07 00:28:19 +0530

    Create GFG-CountSquares.cpp

---
+++ GFG-CountSquares.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+  public:
+    int countSquares(int N) 
+    {
+        int c = 0;
+        for(int i = 1; i < sqrt(N); i++)
+        {
+            if(i*i < N) c++;
+        }
+        return c;
+    }
+};
