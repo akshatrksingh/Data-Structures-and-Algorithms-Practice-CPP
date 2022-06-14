f7c78d6aead3333b73c5fc9d39a6dfb05e46a01c
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-06-14 20:05:10 +0530

    Create GFG-0-1KnapsackProblem.cpp

---
+++ GFG-0-1KnapsackProblem.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    int solve(int W, int wt[], int val[], int n, vector<vector<int>> &dp) 
+    {
+        if(n == 0 || W == 0) return 0;
+        if(dp[n][W] != -1) return dp[n][W];
+        else if(wt[n-1] <= W) return dp[n][W] = max(val[n-1] + solve(W-wt[n-1], wt, val, n-1, dp), solve(W, wt, val, n-1, dp));
+        else if(wt[n-1] > W) return dp[n][W] = solve(W, wt, val, n-1, dp);
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    {
+        vector<vector<int>>dp(n+1, vector<int>(W+1,-1));
+        return solve(W, wt, val, n, dp); 
+    }
+};
