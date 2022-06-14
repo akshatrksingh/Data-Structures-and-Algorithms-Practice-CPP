8137d3c212cb6b557b27a03a2704e5f6486d6bf9
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-06-14 12:42:41 +0530

    Create GFG-PalindromeString.cpp

---
+++ GFG-PalindromeString.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+    public:	
+    	
+    	
+    	int isPalindrome(string S)
+    	{
+    	    for(int i = 0; i < S.size()/2; i++)
+    	    {
+    	        if(S[i] != S[S.size()-1-i]) return 0;
+    	    }
+    	    return 1;
+    	}
+};
