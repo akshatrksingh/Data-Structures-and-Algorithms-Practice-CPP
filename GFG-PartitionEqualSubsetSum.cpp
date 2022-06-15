61c4ce3832d13fb31ac893d87e42bfcd46aef186
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-06-15 19:51:02 +0530

    Update GFG-PartitionEqualSubsetSum.cpp

--- GFG-PartitionEqualSubsetSum.cpp
+++ GFG-PartitionEqualSubsetSum.cpp
@@ -20,6 +20,12 @@ class Solution
                     {
                         dp[i][j] = true;          // {} is possible for sum = 0 if we are summing up nothing 
                     }
+                }
+            }
+            for(int i = 1; i < n+1; i++)
+            {
+                for(int j = 1; j < sum+1; j++)
+                {
                     else if(arr[i-1] <= j)
                     {
                         dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
