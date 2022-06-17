2718d5e71c2fcfb42040bb8c451a4196731473e2
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-06-17 19:05:37 +0530

    Update GFG-0-1KnapsackProblem(BottomUpOrTabulation).cpp

--- GFG-0-1KnapsackProblem(BottomUpOrTabulation).cpp
+++ GFG-0-1KnapsackProblem(BottomUpOrTabulation).cpp
@@ -18,7 +18,7 @@ class Solution
             {
                 for(int j = 1; j < W+1; j++)
                 {
-                    else if(wt[i-1] <= j)
+                    if(wt[i-1] <= j)
                     {
                         dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
                     }
