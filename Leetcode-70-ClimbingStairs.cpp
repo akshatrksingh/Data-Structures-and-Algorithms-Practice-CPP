9093ba57cece5f72bce22b3b62dfb308d53309cf
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-07-07 00:46:39 +0530

    Create Leetcode-70-ClimbingStairs.cpp

---
+++ Leetcode-70-ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public:
+        int climbStairs(int n) 
+        {
+            int dp[n+1];
+            memset(dp, INT_MAX, sizeof(n));
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i = 2; i < n+1; i++)
+            {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n];
+        }
+};
