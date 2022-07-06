64b4760f80e0abbb961baa26534caf5c2f395d76
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-07-06 17:38:37 +0530

    Update GFG-LongestCommonSubsequence(BottomUpOrTabulation).cpp

--- GFG-LongestCommonSubsequence(BottomUpOrTabulation).cpp
+++ GFG-LongestCommonSubsequence(BottomUpOrTabulation).cpp
@@ -24,7 +24,8 @@ class Solution
     public:
     int lcs(int x, int y, string s1, string s2)
     {
-        vector<vector<int>> dp(x+1, vector<int>(y+1,-1));
+        int dp[x+1][y+1];        
+        memset(dp, -1, sizeof(dp));
         for(int i = 0; i < x+1; i++)
         {
             for(int j = 0; j < y+1; j++)
@@ -39,13 +40,13 @@ class Solution
         {
             for(int j = 1; j < y+1; j++)
             {
-                if(s1[i-1] != s2[j-1])
-                {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-                else
+                if(s1[i-1] == s2[j-1])
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else if(s1[i-1] != s2[j-1])
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
