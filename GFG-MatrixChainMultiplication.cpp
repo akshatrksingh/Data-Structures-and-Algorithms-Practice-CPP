dd733eb37242ca50ec519984141354dff007f459
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-07-07 14:24:13 +0530

    Update GFG-MatrixChainMultiplication.cpp

--- GFG-MatrixChainMultiplication.cpp
+++ GFG-MatrixChainMultiplication.cpp
@@ -1,21 +1,23 @@
 class Solution
 {
     public:
+        int dp[101][101];
         int solve(int arr[], int i, int j)
         {
             if(i >= j)
                 return 0;
+            if(dp[i][j] != -1)
+                return dp[i][j];
             int mn = INT_MAX;
             for(int k = i; k < j; k++)
             {
                 int temp_ans = solve(arr, i, k) + solve(arr, k+1, j) + arr[i-1]*arr[k]*arr[j];
                 mn = min(mn, temp_ans);
             }
-            return mn;
+            return dp[i][j] = mn;
         }
         int matrixMultiplication(int n, int arr[])
         {
-            int dp[n+1][n+1];
             memset(dp, -1, sizeof(dp));
             solve(arr, 1, n-1);
         }
