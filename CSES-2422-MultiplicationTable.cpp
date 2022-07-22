86120ed4878f7f5b0bd1f2bec98b1cabc02cadd3
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-07-23 00:14:18 +0530

    Add files via upload

--- CSES-2422-MultiplicationTable.cpp
+++ CSES-2422-MultiplicationTable.cpp
@@ -1,21 +1,28 @@
+// https://usaco.guide/problems/cses-2422-multiplication-table/solution#explanation
 #include <bits/stdc++.h>
 using namespace std;
+
+bool isValid(long long n, long long mid)
+{
+    long long leq = 0;
+    for (int i = 1; i <= n; i++)
+	{
+		leq += min(n, mid/i);
+	}    
+    return leq >= (n*n +1)/2;
+}
 
 int main()
 {
     long long n;
     cin>>n;
-    long long lb = 1, ub = n*n, mid, leq;
+    long long lb = 1, ub = n*n, mid;
     long long res = -1;
     while (lb <=  ub)
 	{
 		mid = lb + (ub-lb)/2;
-        leq = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			leq += min(n, mid/i);
-		}
-		if (leq >= (n*n +1)/2)
+		
+		if(isValid(n, mid))
 		{
             res = mid;
 			ub = mid-1;
