db1b87b4aa4e99b3d01c4f9983dbc949d5e1ea9d
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-07-22 20:57:55 +0530

    Create Lintcode-259-FindTheLargestDivisor.cpp

---
+++ Lintcode-259-FindTheLargestDivisor.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+    public:
+        bool isValid(vector<int> A, int N, int threshold, int max_divisor)
+        {
+            int sum = 0;
+            for(int i = 0; i < N; i++)
+            {
+                sum += ceil(A[i]*1.0/max_divisor);
+            }
+            if(sum < threshold)
+                return false;
+            return true;
+        }
+        int findDivisor(vector<int> &nums, int threshold) 
+        {
+            int lb = 1;
+            int ub = *max_element(nums.begin(), nums.end());
+            int res = -1;
+            while(lb <= ub)
+            {
+                int mid = lb + (ub-lb)/2;
+                if(isValid(nums, nums.size(), threshold, mid))
+                {
+                    res = mid;
+                    lb = mid+1;
+                }
+                else
+                {
+                    ub = mid-1;
+                }
+            }
+            return res;
+        }
+};
