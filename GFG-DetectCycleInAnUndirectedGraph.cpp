22c8a88e2594748a847b71a7d13729f1c6345815
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-07-26 23:36:15 +0530

    Update GFG-DetectCycleInAnUndirectedGraph.cpp

--- GFG-DetectCycleInAnUndirectedGraph.cpp
+++ GFG-DetectCycleInAnUndirectedGraph.cpp
@@ -1,37 +1,41 @@
 class Solution 
 {
   public:
-    bool isCyclicRecursion(int src, bool vis[],  int parent, vector<int> adj[])
+  
+    vector<bool> vis;
+    vector<int> parent;
+    int cycle_start, cycle_end;
+    
+    bool isCyclicRecursion(int src, int par, vector<int> adjList[])
     {
         vis[src] = true;
-        for(auto nbr: adj[src])
+        for(auto nbr: adjList[src])
 		{
-		    if(!vis[nbr])
-		    {
-		        if(isCyclicRecursion(nbr, vis, src, adj))
-		        {
-		            return true;
-		        }
-		     }
-		    else if(nbr != parent)
-		    {
-	            return true;
-	        }
-		 	
+		    if(nbr == par) 
+		    {   
+		        continue; 
+		    }
+            if(vis[nbr]) 
+            {
+                cycle_end = src;
+                cycle_start = nbr;
+                return true;
+            }
+            parent[nbr] = src;
+            if (isCyclicRecursion(nbr, parent[nbr], adjList))
+            {
+                return true;
+            }
 		}
 		return false;
     }
-    bool isCycle(int V, vector<int> adj[]) 
+    bool isCycle(int V, vector<int> adjList[]) 
     {
-        
-        bool *vis = new bool[V];         
+        vis.assign(V, false);
+        parent.assign(V, -1);
         for(int i = 0; i < V; i++)
         {
-            vis[i] = false;
-        }
-        for(int i = 0; i < V; i++)
-        {
-            if(!vis[i] && isCyclicRecursion(i, vis, -1, adj))
+            if(!vis[i] && isCyclicRecursion(i, parent[i], adjList))
             {
                 return true;
             }
