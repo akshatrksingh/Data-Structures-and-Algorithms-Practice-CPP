97c6dfdd385b93d445e03230e351f6ff5b9576ce
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-08-11 20:02:07 +0530

    Update Leetcode-560-SubarraySumEqualsK.cpp

--- Leetcode-560-SubarraySumEqualsK.cpp
+++ Leetcode-560-SubarraySumEqualsK.cpp
@@ -5,13 +5,14 @@ class Solution
         {
             unordered_map<int, int> m;
             int prefSum = 0, ans = 0, x;
+            m[0] = 1;
             for(int i = 0; i < nums.size(); i++)
             {
-                m[prefSum]++;     // m[0] = 1 (why?)
                 prefSum += nums[i];
                 x = prefSum-k;
                 if(m.find(x) != m.end())
                     ans += m[x];
+                m[prefSum]++;     
             }
             return ans;
         }
