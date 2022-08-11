82cbd01a6fa34290bef2b20e548a54b124384691
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-08-11 13:38:42 +0530

    Create Codechef-START51C-SPLITN.cpp

---
+++ Codechef-START51C-SPLITN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t, n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    int ans = 0;
+	    cout<<__builtin_popcount(n)-1<<"\n";
+	}
+	return 0;
+}
