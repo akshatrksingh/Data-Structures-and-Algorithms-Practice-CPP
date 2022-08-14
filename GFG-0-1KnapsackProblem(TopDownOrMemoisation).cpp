efda3aecdee6e8cbb285643a4a028dd0ec8c308e
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-08-15 02:59:00 +0530

    Update GFG-0-1KnapsackProblem(TopDownOrMemoisation).cpp

--- GFG-0-1KnapsackProblem(TopDownOrMemoisation).cpp
+++ GFG-0-1KnapsackProblem(TopDownOrMemoisation).cpp
@@ -1,16 +1,22 @@
 class Solution
 {
     public:
-    int solve(int W, int wt[], int val[], int n, vector<vector<int>> &dp) 
-    {
-        if(n == 0 || W == 0) return 0;
-        else if(dp[n][W] != -1) return dp[n][W];
-        else if(wt[n-1] <= W) return dp[n][W] = max(val[n-1] + solve(W-wt[n-1], wt, val, n-1, dp), solve(W, wt, val, n-1, dp));
-        else if(wt[n-1] > W) return dp[n][W] = solve(W, wt, val, n-1, dp);
-    }
-    int knapSack(int W, int wt[], int val[], int n) 
-    {
-        vector<vector<int>>dp(n+1, vector<int>(W+1,-1));
-        return solve(W, wt, val, n, dp); 
-    }
+        int solve(int index, int wt_left, int wt[], int val[], vector<vector<int>> &dp) 
+        {
+            if(index < 0 || wt_left == 0)
+                return 0;
+            if(dp[index][wt_left] != -1)
+                return dp[index][wt_left];
+            if(wt[index] <= wt_left)    // CHOOSE THE ITEM AT INDEX
+                return dp[index][wt_left] = max(val[index] + solve(index-1, wt_left-wt[index], wt, val, dp),
+                           solve(index-1, wt_left, wt, val, dp));
+            else                        // DO NOT CHOOSE THE ITEM AT INDEX
+                return dp[index][wt_left] = solve(index-1, wt_left, wt, val, dp);
+            
+        }                       
+        int knapSack(int W, int wt[], int val[], int n) 
+        {
+            vector<vector<int>>dp(n+1, vector<int>(W+1,-1));
+            return solve(n-1, W, wt, val, dp);
+        }
 };
