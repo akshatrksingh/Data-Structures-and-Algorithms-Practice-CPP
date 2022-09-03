0fc5d8c2a9dee8a1c6b2bec1e841fefa8f10992b
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-09-03 11:23:41 +0530

    Create Leetcode-21-MergeTwoSortedLists.cpp

---
+++ Leetcode-21-MergeTwoSortedLists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution 
+{
+    public:
+        ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) 
+        {
+            ListNode *m = new ListNode(-1);
+            ListNode *n = m;
+            while(list1 && list2)
+            {
+                if(list1->val < list2->val)
+                {
+                    m->next = list1;
+                    list1 = list1->next;
+                }
+                else
+                {
+                    m->next = list2;
+                    list2 = list2->next;
+                }
+                m = m->next;
+            }
+            if(list1)
+            {
+                m->next = list1;
+            }
+            else if(list2)
+            {
+                m->next = list2;
+            }
+            return n->next;
+        }
+};
