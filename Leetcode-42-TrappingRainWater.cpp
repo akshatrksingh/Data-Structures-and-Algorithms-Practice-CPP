e537bf4be0be869d4ffebdf96f5ee79a9f146975
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-01-23 23:07:02 +0530

    Update Leetcode-42-TrappingRainWater.cpp

--- Leetcode-42-TrappingRainWater.cpp
+++ Leetcode-42-TrappingRainWater.cpp
@@ -28,29 +28,63 @@ class Solution
 };
 */
 
-// Stacks
+/*
 class Solution 
 {
     public:
         int trap(vector<int> &height) 
         {
-            int water = 0, current = 0;
-            stack<int> st;
-            while (current < height.size()) 
+            int n = height.size();
+            vector<int> leftMax(n), rightMax(n);
+            leftMax[0] = 0;
+            rightMax[n-1] = 0;
+            for (int i = 1; i < n; ++i) 
             {
-                while (!st.empty() && height[current] > height[st.top()]) 
+                leftMax[i] = max(height[i-1], leftMax[i-1]);
+            }
+            for (int i = n-2; i >= 0; --i) 
+            {
+                rightMax[i] = max(height[i+1], rightMax[i+1]);
+            }
+            int ans = 0;
+            for (int i = 0; i < n; ++i) 
+            {
+                int waterLevel = min(leftMax[i], rightMax[i]);
+                if (waterLevel >= height[i]) 
+                {
+                    ans += waterLevel - height[i];
+                }
+            }
+            return ans;
+        }
+        // TC: 0(N)
+        // SC: 0(N)
+};
+*/
+
+class Solution 
+{
+    public:
+        int trap(vector<int> &height) 
+        {
+            int n = height.size();
+            int ans = 0, curr = 0;
+            stack<int> st;
+            while(curr < n)
+            {
+                while(!st.empty() && height[st.top()] < height[curr])
                 {
                     int top = st.top();
                     st.pop();
-                    if (st.empty())
+                    if(st.empty())
                         break;
-                    int distance = current-st.top()-1;
-                    int bounded_height = min(height[current], height[st.top()])-height[top];
-                    water += distance*bounded_height;
+                    int distance = curr-st.top()-1;
+                    int bounded_height = min(height[curr], height[st.top()])-height[top];
+                    ans += distance*bounded_height;
                 }
-                st.push(current++);
+                st.push(curr++);
             }
-            return water;
+            return ans;
         }
         // TC: 0(N)
         // SC: 0(N)
