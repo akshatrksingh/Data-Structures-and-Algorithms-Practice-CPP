c152e2f042043422d16d539f031291461edfb7ee
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-01-24 22:32:02 +0530

    Create GFG-ActivitySelection.cpp

---
+++ GFG-ActivitySelection.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    
+        static bool cmp(pair<int, int> a, pair<int, int> b)
+        {
+            return a.second < b.second;
+        }
+        
+        int activitySelection(vector<int> start, vector<int> end, int n)
+        {
+           vector<pair<int, int>> v;
+           for(int i = 0; i < n; i++)
+           {
+               v.push_back({start[i], end[i]});
+           }
+           sort(v.begin(), v.end(), cmp);
+           int cnt = 1;
+           for(int j = 1, i = 0; j < n; j++)
+           {
+               if(v[j].first > v[i].second)
+               {
+                   cnt++;
+                   i = j;
+               }
+           }
+           return cnt;
+        }
+};
