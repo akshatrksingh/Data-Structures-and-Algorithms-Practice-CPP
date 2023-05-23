41a9bf00bc773ddd88a385bcb9e34e473735f5b3
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-05-23 18:40:13 +0530

    Update Leetcode-153-FindMinimumInRotatedSortedArray.cpp

--- Leetcode-153-FindMinimumInRotatedSortedArray.cpp
+++ Leetcode-153-FindMinimumInRotatedSortedArray.cpp
@@ -1,37 +1,28 @@
 class Solution 
 {
     public:
-        int findMin(vector<int> &nums) 
+        int findMin(vector<int>& nums) 
         {
             int n = nums.size();
-            int lb = 0 , mid, ub = n-1;
-            int next, prev;
-            if(nums[lb] < nums[ub] || n == 1)
+            int lb = 0, ub = n - 1;
+
+            if (nums[lb] <= nums[ub]) 
             {
-                return nums[0];
+                return nums[lb];
             }
-            while(lb <= ub)
+
+            while (lb < ub) 
             {
-                mid = lb + (ub-lb)/2;
-                prev = (mid-1+n)%n;
-                next = (mid+1)%n;
-                if(nums[mid] < nums[prev])
+                int mid = lb+(ub-lb)/2;
+                if (nums[mid] < nums[ub]) 
                 {
-                    return nums[mid];
-                }
-                else if(nums[mid] > nums[next])
-                {
-                    return nums[next];
-                }
-                else if(nums[lb] < nums[mid])    // remove the sorted half as min lies in the unsorted half
+                    ub = mid;
+                } 
+                else 
                 {
                     lb = mid+1;
-                }
-                else if(nums[mid] < nums[ub])
-                {
-                    ub = mid-1;
-                }
+                } 
             }
-            return 0;
+            return nums[lb];
         }
 };
