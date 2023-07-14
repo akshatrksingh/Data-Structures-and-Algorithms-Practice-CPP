cd7a50c8c16307774093fbbcbce30cf5f2f315da
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-07-14 20:55:09 +0530

    Update Leetcode-137-SingleNumberII.cpp

--- Leetcode-137-SingleNumberII.cpp
+++ Leetcode-137-SingleNumberII.cpp
@@ -10,10 +10,14 @@ class Solution
                 for (int num : nums) 
                 {
                     if ((num >> i) & 1)
+                    {
                         count++;
+                    }
                 }
                 if (count % 3 != 0)
+                {
                     result |= (1 << i);
+                }
             }
             return result;
         }
