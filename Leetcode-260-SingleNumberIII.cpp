a7d4c74205f3ff97801d02ddb85995c18e5762fa
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-07-14 21:23:19 +0530

    Update Leetcode-260-SingleNumberIII.cpp

--- Leetcode-260-SingleNumberIII.cpp
+++ Leetcode-260-SingleNumberIII.cpp
@@ -1,48 +1,33 @@
 class Solution 
 {
     public:
-        vector<int> singleNumber(vector<int> &nums) 
+        vector<int> singleNumber(vector<int>& nums) 
         {
-            int XOR = 0, idx;  
-            for(int i: nums)
+            int xorResult = 0;
+            for (int num : nums) 
             {
-                XOR ^= i;
+                xorResult ^= num;
             }
-            /*
-            XOR contains a^b where and b are the required numbers
-            The bit 0 in XOR would imply a and b have the same digit at that place
-            The bit 1 in XOR would imply a and b have different digits at that place
-            There will be atleast one index where they differ because they are not the same
-            Hence XOR will contain the bit 1 atleast once
-            We could consider any such occurence of bit 1 to extract a and b
-            Here we consider the rightmost such set bit
-            */
-            for(int i = 0; i < 32; i++)
+
+            int rightmostSetBit = 1;
+            while ((rightmostSetBit & xorResult) == 0) 
             {
-                if(XOR&(1<<i))  // Test if the ith bit is set
+                rightmostSetBit <<= 1;                  // Left shift to find the next set bit
+            }
+
+            int num1 = 0, num2 = 0;
+            for (int num : nums) 
+            {
+                if ((num & rightmostSetBit) != 0) 
                 {
-                    idx = i;
-                    break;
+                    num1 ^= num;
+                } 
+                else 
+                {
+                    num2 ^= num;
                 }
             }
-            
-            /* 
-            a and b differ at this set bit of XOR
-            Only 1 of the numbers out of a and b would have bit 1 at idx 
-            Let that number be a WLOG
-            XOR only numbers where the bit at idx is set i.e. 1
-            Thus we extract a
-            */
-            int a = 0;
-            for(int i = 0; i < nums.size(); i++)
-            {
-                if(nums[i]&(1<<idx))  // Test if the ith bit is set
-                {
-                    a ^= nums[i];
-                }
-            }
-            
-            int b = a^XOR;
-            return {a, b};
+
+            return {num1, num2};
         }
 };
