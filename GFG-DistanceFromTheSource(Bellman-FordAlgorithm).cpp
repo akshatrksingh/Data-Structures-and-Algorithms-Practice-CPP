965a00fa8bfe3c3d8ea42bb8bd9592054e0fcf30
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-07-31 23:42:04 +0530

    Update GFG-DistanceFromTheSource(Bellman-FordAlgorithm).cpp

--- GFG-DistanceFromTheSource(Bellman-FordAlgorithm).cpp
+++ GFG-DistanceFromTheSource(Bellman-FordAlgorithm).cpp
@@ -1,18 +1,42 @@
 class Solution
 {
-	public:
-    vector <int> bellman_ford(int V, vector<vector<int>> edgeList, int src) 
-    {
-       const int INF = 1e8; // INF = 100M, not 2^31-1 to avoid overflow
-       vector<int> dist(V, INF);
-       dist[src] = 0;
-       for(int i = 0; i < V-1; i++)
-       {
-           for(auto edge: edgeList)        // edge: {u, v, weight}
-           {
-               dist[edge[1]] = min(dist[edge[1]], dist[edge[0]]+edge[2]); 
-           }
-       }
-       return dist;
-    }
+    public:
+        vector<int> bellman_ford(int V, vector<vector<int>>& edgeList, int src)
+        {
+            const int INF = 1e8; 
+            vector<int> dist(V, INF);
+            dist[src] = 0;
+            
+            // Relax all edges V - 1 times
+            for (int i = 0; i < V - 1; ++i)
+            {
+                for (const auto& edge : edgeList)
+                {
+                    int u = edge[0];
+                    int v = edge[1];
+                    int weight = edge[2];
+                    
+                    if (dist[u] + weight < dist[v])
+                    {
+                        dist[v] = dist[u] + weight;
+                    }
+                }
+            }
+            
+            // Check for negative cycles
+            for (const auto& edge : edgeList)
+            {
+                int u = edge[0];
+                int v = edge[1];
+                int weight = edge[2];
+                
+                if (dist[u] + weight < dist[v])
+                {
+                    // Negative cycle found, return -1
+                    return {-1};
+                }
+            }
+            
+            return dist;
+        }
 };
