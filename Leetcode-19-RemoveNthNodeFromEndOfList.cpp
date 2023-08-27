3cd5732ae19e63e615f56b2acdf7cd70b1f37921
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2023-08-28 00:26:05 +0530

    Update Leetcode-19-RemoveNthNodeFromEndOfList.cpp

--- Leetcode-19-RemoveNthNodeFromEndOfList.cpp
+++ Leetcode-19-RemoveNthNodeFromEndOfList.cpp
@@ -8,12 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/*
+class Solution 
+{
+    public:
+        ListNode* removeNthFromEnd(ListNode* head, int n) 
+        {
+            if(head == NULL)
+            {
+                return head;
+            }
+            int len = 0;
+            ListNode* curr = head;
+            while(curr != NULL)
+            {
+                len++;
+                curr = curr->next;
+            }
+            int k = len-n;
+            ListNode* dummy = new ListNode(-1);
+            dummy->next = head;
+            ListNode* prev = dummy;
+            while(k--)
+            {
+                prev = prev->next;
+            }
+            ListNode* temp;
+            temp = prev->next;
+            prev->next = temp->next;
+            temp->next = NULL;
+            delete temp;
+            return dummy->next;
+        }
+};
+*/
 class Solution
 {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n)
     {
-        ListNode* dummy = new ListNode(0);
+        ListNode* dummy = new ListNode(-1);
         dummy->next = head;
         ListNode* slow = dummy;
         ListNode* fast = head;
